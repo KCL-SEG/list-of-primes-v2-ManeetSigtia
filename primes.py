82201d8fe0e0c20114a5793f9659ce392df3c8b7
Maneet Sigtia <maneet.sigtia@kcl.ac.uk>
2023-09-26 18:53:23 +0100

    Complete primes

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    number = 2
+    found_primes = 1
+    primes = [2]
+
+    if number_of_primes <= 0:
+        raise ValueError("Please enter a positive number")
+
+    while found_primes < number_of_primes:
+        if is_divisible(number, primes):
+            primes.append(number)
+            found_primes += 1
+        number += 1
+
+    return primes
+
+
+def is_divisible(number, primes):
+    composite = True
+    for i in primes:
+        if number % i == 0:
+            composite = False
+            break
+    return composite
